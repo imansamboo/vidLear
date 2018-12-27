9163810b57c6d14db6cb28419cfbebc27f4eafc6
imansamboo <imansamboo@gmail.com>
2018-12-28 02:04:20 +0330

    first commit

--- public/js2/register.js
+++ public/js2/register.js
@@ -30,7 +30,9 @@ $(document).ready(function() {
                     '        <div class="modal-content">\n' +
                     '            <div class="modal-header">\n' +
                     '                <button type="button" class="close" data-dismiss="modal">×</button>\n' +
-                    '                <h5 class="modal-title">لطفا کد ارسالی را جهت تایید نهایی وارد نمایید.</h5>\n' +
+                    '                <h5 class="modal-title">لطفا کد ارسالی را جهت تایید نهایی وارد نمایید.</h5>' +
+                    '                <div id="sms-verify-error" >\n' +
+                    '                </div>\n' +
                     '            </div>\n' +
                     '            <div class="modal-body">\n' +
                     '                <p id="message"></p>\n' +
@@ -78,15 +80,56 @@ $(document).ready(function() {
                         },
                         error: function () {
                             console.log('error sms varify');
+                            $('div.alert-danger').hide();
+                            $('div#sms-verify-error').append('<div class="alert alert-danger" style="font-size: 11px">\n' +
+                                '                         کد وارد شده صحیح نیست.\n' +
+                                '                    </div>');
+                            $("div#sms-verify-error").css('padding-top', '5%');
                         }
                     })
 
                 })
             },
             error: function(xhr, status, error){
-                console.log(error)
+                /*console.log(error)
                 console.log(xhr)
-                //var err = eval("(" + xhr.responseText + ")");
+                var err = eval("(" + xhr.responseText + ")");
+                console.log(err.errors);
+                console.log(err.errors.password.length);*/
+                var err = eval("(" + xhr.responseText + ")");
+                $('div.alert-danger').hide();
+                for (x in err.errors) {
+                    if( x == 'password'){
+                        if(err.errors[x].length > 1){
+                            $('div#password-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                '            رمز عبور وارد شده کمتر از چهار کاراکتر میباشد.\n' +
+                                '        </div>');
+                            $('div#passwordConfirm-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                '            رمزهای عبور وارد شده مطابقت ندارند.\n' +
+                                '        </div>');
+                        }else{
+                            if(err.errors[x][0] == "The email must be a valid email address."){
+                                $('div#password-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                    '            رمز عبور وارد شده کمتر از چهار کاراکتر میباشد.\n' +
+                                    '        </div>');
+                            }else if(err.errors[x][0] == "The password confirmation does not match.") {
+                                $('div#passwordConfirm-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                    '            رمزهای عبور وارد شده مطابقت ندارند.\n' +
+                                    '        </div>');
+                            }
+                        }
+                    }else if(x == 'email'){
+                        if(err.errors[x][0] ==  "The email has already been taken."){
+                            $('div#email-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                '            این پست الکترونیک قبلا استفاده شده است.\n' +
+                                '        </div>');
+                        }else if(err.errors[x][0] ==  "The email must be a valid email address.") {
+                            $('div#email-error').append('<div class="alert alert-danger" style="font-size: 9px; padding: 5px; margin-bottom: 7px">\n' +
+                                '            فرمت پست الکترونیک وارد شده صحیح نمی باشد.\n' +
+                                '        </div>');
+                        }
+                    }
+                }
 
                 //console.log(err.errors);
             }
